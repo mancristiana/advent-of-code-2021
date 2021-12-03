7fa16ba8adaffd85eacea0a97bdfec354ab0a802
Cristiana Man <man.cristiana1@gmail.com>
2021-12-03 13:40:27 +0100

    Simplify one

--- src/day-03-binary-diagnostic/one.js
+++ src/day-03-binary-diagnostic/one.js
@@ -1,32 +1,31 @@
 import { URL, fileURLToPath } from 'url'
 import { readInput } from '../utils/readInput.js'
 
+const getMostCommonBit = (lines, index) => {
+  let occurenceDifference = 0
+  lines.forEach((line) => {
+    const bit = line.charAt(index)
+    if (bit === '0') {
+      occurenceDifference += 1
+    } else {
+      occurenceDifference -= 1
+    }
+  })
+  const mostCommonBit = occurenceDifference > 0 ? '0' : '1'
+  return mostCommonBit
+}
+
 const solve = (data) => {
   const lines = data.split('\n')
   const numberOfBits = lines[0].length
-  const occurenceDifferences = Array(numberOfBits).fill(0)
   let gammaRate = ''
   let epsilonRate = ''
 
-  lines.forEach((line) => {
-    line.split('').forEach((bit, index) => {
-      if (bit === '0') {
-        occurenceDifferences[index] += 1
-      } else {
-        occurenceDifferences[index] -= 1
-      }
-    })
-  })
-
-  occurenceDifferences.forEach((occurenceDifference) => {
-    if (occurenceDifference > 0) {
-      gammaRate += '0'
-      epsilonRate += '1'
-    } else {
-      gammaRate += '1'
-      epsilonRate += '0'
-    }
-  })
+  for (let i = 0; i < numberOfBits; i++) {
+    const mostCommonBit = getMostCommonBit(lines, i)
+    gammaRate += mostCommonBit
+    epsilonRate += mostCommonBit === '0' ? '1' : '0'
+  }
 
   return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2)
 }
