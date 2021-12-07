47191fa04a1ed947344e9cde7ab1d8560ebc10eb
Cristiana Man <man.cristiana1@gmail.com>
2021-12-07 08:17:35 +0100

    Day 7

---
+++ src/day-07-the-treachery-of-whales/two.js
@@ -0,0 +1,36 @@
+import { URL, fileURLToPath } from 'url'
+import { readInput } from '../utils/readInput.js'
+
+const calculateFuelForSteps = (n) => (n * (n + 1)) / 2
+
+const calculateFuel = (positions, alignPos) => {
+  return positions.reduce(
+    (fuel, pos) => fuel + calculateFuelForSteps(Math.abs(pos - alignPos)),
+    0
+  )
+}
+
+const solve = (data) => {
+  const positions = data.split(',').map((n) => Number(n))
+  const [minAlignPos, maxAlignPos] = positions.reduce(
+    ([min, max], pos) => [Math.min(min, pos), Math.max(max, pos)],
+    [positions[0], positions[0]]
+  )
+
+  let minFuel
+
+  for (let alignPos = minAlignPos; alignPos <= maxAlignPos; alignPos++) {
+    const fuel = calculateFuel(positions, alignPos)
+    if (!minFuel || fuel < minFuel) {
+      minFuel = fuel
+    }
+  }
+
+  return minFuel
+}
+
+const inputPath = fileURLToPath(new URL('./input.txt', import.meta.url))
+
+const data = await readInput(inputPath)
+
+console.log(solve(data))
